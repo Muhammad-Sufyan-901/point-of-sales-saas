b74881d575fb70ff7bbe99fc50cc5ad353aad35b
Muhammad Sufyan <108451771+Muhammad-Sufyan-901@users.noreply.github.com>
2026-04-03 23:09:28 +0700

    feat : adding all reusable components

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,4 +1,6 @@
 import { createInertiaApp } from '@inertiajs/react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+import { TooltipProvider } from '@/Core/Components/ui/tooltip';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -6,5 +8,23 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     progress: {
         color: '#4B5563',
+    },
+    setup({ el, App, props }) {
+        if (import.meta.env.SSR) {
+            hydrateRoot(
+                el!,
+                <TooltipProvider>
+                    <App {...props} />
+                </TooltipProvider>,
+            );
+
+            return;
+        }
+
+        createRoot(el!).render(
+            <TooltipProvider>
+                <App {...props} />
+            </TooltipProvider>,
+        );
     },
 });
